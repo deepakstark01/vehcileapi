44cecc07c646e3ab32dbfd1ecc9b892878965b5e
Deepak <rohit45deepak@gmail.com>
2024-05-24 19:17:17 +0530

    done

--- main.py
+++ main.py
@@ -116,3 +116,6 @@ def home(vehNum):
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='80', debug=True)
